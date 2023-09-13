6138f5fe98b07cf1ed4b613e72ba82eab3300f3a
Zohaifa <141559676+Zohaifa@users.noreply.github.com>
2023-09-13 15:56:22 +0600

    Create T.c

---
+++ T.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, j, k = 1;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n-i; j++){
+            printf(" ");
+        }
+        for(j=1; j<=k; j++){
+            printf("*");
+        }
+        k+=2;
+        printf("\n");
+    }
+
+    return 0;
+}
